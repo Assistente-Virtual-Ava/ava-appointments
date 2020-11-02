55d3f2c856a26271779f96c2c253a3dc7cdecee5
Anderson Almeida <andi.r.almeida@gmail.com>
2020-11-02 11:20:55 -0300

    test only access control header

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
 header("Access-Control-Allow-Origin: *");
+// header('content-type: application/json; charset=utf-8');
 /**
  * CodeIgniter
  *
