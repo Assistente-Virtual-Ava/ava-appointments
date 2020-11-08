3680d58dd9972fcd687f8ce39ab6ed37d60369c7
labarruffe <lucas_barruffe@hotmail.com>
2020-11-08 17:06:48 -0300

    Add lines that was deleted before

--- assets/sql/structure.sql
+++ assets/sql/structure.sql
@@ -1,3 +1,6 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 CREATE TABLE IF NOT EXISTS `ea_appointments` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `book_datetime` DATETIME,
